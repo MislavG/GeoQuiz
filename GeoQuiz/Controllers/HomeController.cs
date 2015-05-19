d6286626797491f0da0fb837a9b221a716f786bf
AnaMatic <maticana93@gmail.com>
2015-05-19 13:54:32 +0200

    poslana verzija

--- GeoQuiz/Controllers/HomeController.cs
+++ GeoQuiz/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace GeoQuiz.Controllers
     {
         private GeoQuizEntities db = new GeoQuizEntities();
         Random rand = new Random();
-        List<string> drzave = new List<string>();
+        //List<string> drzave = new List<string>();
         [HttpGet] 
         public ActionResult Index()
         {
@@ -24,53 +24,25 @@ namespace GeoQuiz.Controllers
         [HttpGet]
         public ActionResult Play()
         {
-            Random rand = new Random();
-            List<string> drzave = new List<string>();
-            drzave.Add("Croatia");
-            drzave.Add("Serbia & Montenegro");
-            drzave.Add("Bosnia & Herzegovina");
-            drzave.Add("Germany");
-            drzave.Add("Switzerland");
-            int r = rand.Next(drzave.Count);
-
-            /*ViewBag.ListDrzave = drzave;
-            ViewBag.Random = ((string)drzave[r]); //OVO JE DRZAVA KOJA SE SALJE U Play.cshtml U 20. LINIJU KODA
-            ViewBag.Message = "Test lista";
-            Console.WriteLine(ViewBag.Random); 
+            return View(new CheckContinent());
+        }
+        
+        [WebMethod]
+        public string RandomDrzava2()
+        {
             var ids = from drzava in db.Drzava
                       select drzava.NazivDrzavaEng;
             List<string> drzave = new List<string>();
-            Random rand = new Random();*/
-            /*foreach (var item in ids)
+            
+            foreach (var item in ids)
             {
                 drzave.Add(item.Trim());
 
             }
-            int r = rand.Next(drzave.Count);*/
-            ViewBag.Drz = ((string)drzave[r]);
-
-            
-
-            return View(new CheckContinent());
-        }
-
-        [ChildActionOnly]
-        public ActionResult RandomDrzava()
-        {
-            /*var ids = from drzava in db.Drzava
-                      select drzava.NazivDrzavaEng;*/
-            List<string> drzave = new List<string>();
-            Random rand = new Random();
-            /*foreach (var item in ids)
-            {
-                drzave.Add(item);
-
-            }*/
             int r = rand.Next(drzave.Count);
             string odabrana = ((string)drzave[r]);
             drzave.Remove(odabrana);
-            ViewBag.Drz = odabrana;
-            return View();
+            return odabrana;
         }
 
         [HttpPost]
@@ -100,28 +72,6 @@ namespace GeoQuiz.Controllers
 
             return View();
         }
-        [WebMethod]
-        public string RandomDrzava2()
-        {
-            List<string> drzave = new List<string>();
-            drzave.Add("Croatia");
-            drzave.Add("Serbia & Montenegro");
-            drzave.Add("Bosnia & Herzegovina");
-            drzave.Add("Germany");
-            drzave.Add("Switzerland");
-            //Random rand = new Random();
-            int r = rand.Next(drzave.Count);
-            string odabrana = ((string)drzave[r]);
-           /* ScriptManager.RegisterStartupScript(this,
-                                                        this.GetType(),
-                                                        "Funct",
-                                                        "drz = " + odabrana + ";",
-                                                        true);*/
-
-            //return Content(odabrana, "text/html");
-            //return Json(odabrana, JsonRequestBehavior.AllowGet);
-            ViewBag.Drz = odabrana;
-            return odabrana;
-        }
+        
     }
 }
