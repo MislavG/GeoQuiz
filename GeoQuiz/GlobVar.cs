c1b220a4165fb30b2719e90b53badd8373887ba6
AnaMatic <maticana93@gmail.com>
2015-05-20 11:48:12 +0200

    trs trs trs

---
+++ GeoQuiz/GlobVar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeoQuiz
+{
+    public class GlobVar
+    {
+        public static string val = "aa";
+    }
+}
